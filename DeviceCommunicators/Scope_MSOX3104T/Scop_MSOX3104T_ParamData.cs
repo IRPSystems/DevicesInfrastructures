b6d1203b694e050a6e7bf69ece9eeb22dab66532
EvgenyGesisIRP <evgeny@irpsystems.com>
2023-11-29 16:12:46 +0200

    update parameter class

--- DeviceCommunicators/Scope_MSOX3104T/Scop_MSOX3104T_ParamData.cs
+++ DeviceCommunicators/Scope_MSOX3104T/Scop_MSOX3104T_ParamData.cs
@@ -1,17 +1,18 @@
 ﻿using Entities.Models;
 using System.Collections.Generic;
+using System.Security.RightsManagement;
 
 namespace DeviceCommunicators.Scop_MSOX3104T
 {
-     public class WT1804E_ParamData : DeviceParameterData, IParamWithDropDown
+     public class Scop_MSOX3104T_ParamData : DeviceParameterData, IParamWithDropDown
 
     {
-
+        public string User_command {  get; set; } 
         public string Command { set; get; }
+        public string Status_paramter { set; get; }// Read only (R) or write (RW)
         public string data { set; get; } // 0 - not return  result , Number- How many parameters return from the switch relay
         public int Channel { set; get; }
-        public int value { set; get; }
-
+        
         public List<DropDownParamData> DropDown { get; set; }
     }
 }
