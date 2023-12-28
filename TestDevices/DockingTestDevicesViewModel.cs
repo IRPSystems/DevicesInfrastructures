b02dc240938644c1c39f764cdd5bf26fb2fd28c8
Smadar Tsdaka <smadar@irpsystems.com>
2023-12-28 09:13:23 +0200

    Add TestDevices

---
+++ TestDevices/DockingTestDevicesViewModel.cs
@@ -0,0 +1,95 @@
+﻿using Controls.ViewModels;
+using DeviceHandler.ViewModels;
+using DeviceHandler.Views;
+using DeviceSimulators.ViewModels;
+using DeviceSimulators.Views;
+using Syncfusion.Windows.Tools.Controls;
+using System;
+using System.IO;
+using System.Windows.Controls;
+
+namespace TempLoggerViewer.ViewModels
+{
+	public class DockingTestDevicesViewModel : DocingBaseViewModel
+	{
+		#region Fields
+
+		private ContentControl _communicationSettings;
+		private ContentControl _deviceSimulatorsViewModel;
+
+		#endregion Fields
+
+		#region Constructor
+
+		public DockingTestDevicesViewModel(
+			CommunicationViewModel communicationSettings,
+			DeviceSimulatorsViewModel deviceSimulatorsViewModel) :
+			base("DockingMain")
+		{
+
+			CreateWindows(
+				communicationSettings,
+				deviceSimulatorsViewModel);
+		}
+
+		#endregion Constructor
+
+		#region Methods
+
+		private void CreateWindows(
+			CommunicationViewModel communicationSettings,
+			DeviceSimulatorsViewModel deviceSimulatorsViewModel)
+		{
+			DockFill = true;
+
+
+
+
+			_communicationSettings = new ContentControl();
+			CommunicationView communication = new CommunicationView() { DataContext = communicationSettings };
+			_communicationSettings.Content = communication;
+			SetHeader(_communicationSettings, "Communication Settings");
+			SetFloatParams(_communicationSettings);
+			Children.Add(_communicationSettings);
+
+			_deviceSimulatorsViewModel = new ContentControl();
+			DeviceSimulatorsView deviceSimulators = new DeviceSimulatorsView() { DataContext = deviceSimulatorsViewModel };
+			_deviceSimulatorsViewModel.Content = deviceSimulators;
+			SetHeader(_deviceSimulatorsViewModel, "Device Simulators");
+			SetFloatParams(_deviceSimulatorsViewModel);
+			Children.Add(_deviceSimulatorsViewModel);
+
+		}
+
+
+		private void SetFloatParams(ContentControl control)
+		{
+			SetSizetoContentInDock(control, true);
+			SetSizetoContentInFloat(control, true);
+			SetState(control, DockState.Hidden);
+		}
+
+
+		public void OpenCommSettings()
+		{
+			SetState(_communicationSettings, DockState.Float);
+		}
+
+		public void OpenDeviceSimulators()
+		{
+			SetState(_deviceSimulatorsViewModel, DockState.Float);
+		}
+
+
+		public void RestorWindowsLayout()
+		{
+			string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+			path = Path.Combine(path, "TestDevices");
+			path = Path.Combine(path, "Default.txt");
+			if (System.IO.File.Exists(path))
+				LoadDockState(path);
+		}
+
+		#endregion Methods
+	}
+}
---
+++ TestDevices/TestDevicesMainViewModel.cs
@@ -0,0 +1,169 @@
+﻿
+using CommunityToolkit.Mvvm.Input;
+using DeviceCommunicators.Enums;
+using DeviceCommunicators.Interfaces;
+using DeviceCommunicators.Models;
+using DeviceCommunicators.Services;
+using DeviceHandler.Models;
+using DeviceHandler.Models.DeviceFullDataModels;
+using DeviceHandler.ViewModels;
+using Entities.Enums;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Threading;
+using TempLoggerViewer.ViewModels;
+using DeviceSimulators.ViewModels;
+using DeviceSimulators.Views;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace TestDevices
+{
+	public class TestDevicesMainViewModel
+	{
+		#region Properties
+
+		public DevicesContainer DevicesContainter { get; set; }
+		public DockingTestDevicesViewModel Docking { get; set; }
+		public DeviceData SelectedDevice { get; set; }
+
+		public string Version { get; set; }
+
+		#endregion Properties
+
+		#region Fields
+
+		#endregion Fields
+
+		#region Constructor
+
+		public TestDevicesMainViewModel()
+		{
+			Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+			InitDevicesContainter();
+
+			CommunicationViewModel communicationSettings =
+				new CommunicationViewModel(DevicesContainter);
+			DeviceSimulatorsViewModel deviceSimulatorsViewModel = 
+				new DeviceSimulatorsViewModel(DevicesContainter);
+
+			Docking = new DockingTestDevicesViewModel(
+				communicationSettings, 
+				deviceSimulatorsViewModel);
+
+			CommunicationSettingsCommand = new RelayCommand(InitCommunicationSettings);
+			ClosingCommand = new RelayCommand<CancelEventArgs>(Closing);
+			DeviceSimulatorCommand = new RelayCommand(DeviceSimulator);
+
+
+		}
+
+		#endregion Constructor
+
+		#region Methods
+
+		private void Closing(CancelEventArgs e)
+		{
+		}
+
+		private void InitDevicesContainter()
+		{
+			DevicesContainter = new DevicesContainer();
+			DevicesContainter.DevicesFullDataList = new ObservableCollection<DeviceFullData>();
+			DevicesContainter.DevicesList = new ObservableCollection<DeviceData>();
+			DevicesContainter.TypeToDevicesFullData = new Dictionary<DeviceTypesEnum, DeviceFullData>();
+
+			string path = Directory.GetCurrentDirectory();
+			path = Path.Combine(path, "Device Communications");
+
+			ReadDevicesFileService readDevicesFile = new ReadDevicesFileService();
+			ObservableCollection<DeviceData> deviceList = readDevicesFile.ReadAllFiles(
+				path,
+				Path.Combine(path, "param_defaults.json"),
+				Path.Combine(path, "param_defaults.json"),
+				Path.Combine(path, "Dyno Communication.json"),
+				Path.Combine(path, "NI_6002.json"));
+
+			foreach (DeviceFullData device in DevicesContainter.DevicesFullDataList)
+			{
+				device.Disconnect();
+			}
+
+			DevicesContainter.DevicesFullDataList.Clear();
+			DevicesContainter.DevicesList.Clear();
+			DevicesContainter.TypeToDevicesFullData.Clear();
+
+
+			foreach (DeviceData device in deviceList)
+			{
+				DeviceFullData deviceFullData = DeviceFullData.Factory(device);
+
+				deviceFullData.Init();
+
+				DevicesContainter.DevicesFullDataList.Add(deviceFullData);
+				DevicesContainter.DevicesList.Add(device);
+				if (DevicesContainter.TypeToDevicesFullData.ContainsKey(device.DeviceType) == false)
+					DevicesContainter.TypeToDevicesFullData.Add(device.DeviceType, deviceFullData);
+			}
+
+			foreach (DeviceFullData device in DevicesContainter.DevicesFullDataList)
+			{
+				device.Connect();
+				device.InitCheckConnection();
+			}
+		}
+
+		private void InitCommunicationSettings()
+		{
+			Docking.OpenCommSettings();
+		}
+
+		private void DeviceSimulator()
+		{
+			Docking.OpenDeviceSimulators();
+		}
+
+		private void SearchText_TextChanged(TextChangedEventArgs e)
+		{
+			if (!(e.Source is TextBox textBox))
+				return;
+
+			if (!(textBox.DataContext is DeviceData deviceData))
+				return;
+
+			foreach (DeviceParameterData param in deviceData.ParemetersList)
+			{
+				if (param.Name.ToLower().Contains(textBox.Text.ToLower()))
+					param.Visibility = Visibility.Visible;
+				else
+					param.Visibility = Visibility.Collapsed;
+			}
+		}
+
+		#endregion Methods
+
+		#region Commands
+
+		public RelayCommand CommunicationSettingsCommand { get; private set; }
+		public RelayCommand<CancelEventArgs> ClosingCommand { get; private set; }
+		public RelayCommand DeviceSimulatorCommand { get; private set; }
+
+
+		private RelayCommand<TextChangedEventArgs> _SearchText_TextChangedCommand;
+		public RelayCommand<TextChangedEventArgs> SearchText_TextChangedCommand
+		{
+			get
+			{
+				return _SearchText_TextChangedCommand ?? (_SearchText_TextChangedCommand =
+					new RelayCommand<TextChangedEventArgs>(SearchText_TextChanged));
+			}
+		}
+
+		#endregion Commands
+	}
+}
