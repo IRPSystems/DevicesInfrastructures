db3238d6fc583f4f01534f78784bca93d06e5415
Smadar Tsdaka <smadar@irpsystems.com>
2023-12-28 13:32:51 +0200

    Add SelectedParametersListView

---
+++ DeviceHandler/Models/RecordData.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using DeviceCommunicators.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceHandler.Models
+{
+	public class RecordData : ObservableObject
+	{
+		public int Index { get; set; }
+		public DeviceParameterData Data { get; set; }
+	}
+}
