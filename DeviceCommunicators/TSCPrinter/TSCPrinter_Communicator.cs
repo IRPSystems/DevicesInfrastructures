1a68c0065f9affeb43dd9f3fc8046c851be9e56d
Smadar Tsdaka <smadar@irpsystems.com>
2024-02-15 10:22:27 +0200

    Removed old code

--- DeviceCommunicators/TSCPrinter/TSCPrinter_Communicator.cs
+++ DeviceCommunicators/TSCPrinter/TSCPrinter_Communicator.cs
@@ -15,19 +15,11 @@ namespace DeviceCommunicators.TSCPrinter
 	{
 		#region Fields
 
-		
-		//private string _name_comport;
-		//private int _boud_rate;
 
 		#endregion Fields
 
 
 		#region Properties
-
-		private ISerialService _serial_port
-        {
-            get => CommService as ISerialService;
-        }
 
 
 		#endregion Properties
@@ -36,7 +28,7 @@ namespace DeviceCommunicators.TSCPrinter
 
 		public TSCPrinter_Communicator()
 		{
-			_parameterQueue = new BlockingCollection<CommunicatorIOData>();
+			
 		}
 
 		#endregion Constructor
@@ -51,19 +43,7 @@ namespace DeviceCommunicators.TSCPrinter
 			int txPort = 0,
 			string address = "")
         {
-            //_name_comport = comName;
-            //_boud_rate = baudtate;
-
-            if(isUdpSimulation)
-				CommService = new SerialUdpSimulationService(rxPort, txPort, address);
-            else
-				CommService = new SerialService(comName, baudtate);
             
-            _serial_port.Init(false);
-
-            if(_serial_port.IsInitialized)
-                _serial_port.Send("SYST:REM\n");
-
 
 
 			InitBase();
@@ -72,11 +52,7 @@ namespace DeviceCommunicators.TSCPrinter
 
 		public override void Dispose()
 		{
-            if (_serial_port != null)
-            {
-                if(_serial_port.IsInitialized)
-                    _serial_port.Send("SYST:LOC\n");
-            }
+            
 
 			base.Dispose();
 		}
@@ -110,11 +86,10 @@ namespace DeviceCommunicators.TSCPrinter
 		{ 
             try
             {
-                if (!(param is PowerSupplayBK_ParamData supplay_Parameter))
+                if (!(param is TSCPrinter_ParamData tscPrinter_Param))
                     return;
 
-                send_comand_to_supply(supplay_Parameter.Name, value);
-				callback?.Invoke(param, CommunicatorResultEnum.OK, null);
+                
 			}
 			catch (Exception ex)
 			{
@@ -128,20 +103,10 @@ namespace DeviceCommunicators.TSCPrinter
 		{ 
             try
             {
-                if (!(param is PowerSupplayBK_ParamData supplay_Parameter))
+                if (!(param is TSCPrinter_ParamData tscPrinter_Param))
                     return;
 
-                request_commad_from_supply(supplay_Parameter.Name);
-
-                Thread.Sleep(10);
-
-
-                param.Value = receive_value_from_supply(supplay_Parameter.Name);
-
-                if(param.Value == null)
-                	callback?.Invoke(param, CommunicatorResultEnum.NoResponse, null);
-				else
-                    callback?.Invoke(param, CommunicatorResultEnum.OK, null);
+                
             }
             catch(Exception ex) 
             { 
@@ -150,98 +115,6 @@ namespace DeviceCommunicators.TSCPrinter
 		}
 
 		
-
-
-
-		private void send_comand_to_supply(string command, double value)
-        {
-            if (_serial_port == null)
-                return;
-
-            if (command == "Remote command")
-            {
-                if (value == 1)
-                {
-					_serial_port.Send("SYST:REM");
-                }
-                else if (value == 0)
-                {
-					_serial_port.Send("SYST:LOC");
-                }
-            }
-            else if (command == "Choose channel")
-            {
-                if (value == 1)
-                {
-					_serial_port.Send("INST CH1");
-                }
-                else if (value == 2)
-                {
-					_serial_port.Send("INST CH2");
-                }
-                else if (value == 3)
-                {
-					_serial_port.Send("INST CH3");
-                }
-            }
-            else if (command == "Turn ON channel")
-            {
-                if (value == 0)
-                {
-					_serial_port.Send("CHANnel:OUTPut OFF");
-                }
-                else if (value == 1)
-                {
-					_serial_port.Send("CHANnel:OUTPut ON");
-                }
-            }
-            else if (command == "Voltage")
-            {
-				_serial_port.Send("VOLTage "+ value);
-            }
-            else if (command == "Current")
-            {
-				_serial_port.Send("Current " + value);
-            }
-        }
-
-		private void request_commad_from_supply(string name)
-        {
-            if (_serial_port == null)
-                return;
-
-            if (name == "MEASure voltage in supply")
-            {
-                _serial_port.Send("MEASure:SCALar:VOLTage:ALL:DC?");
-            }
-            if (name == "Voltage in supply")
-            {
-                _serial_port.Send("VAPPLY:VOLTage:LEVel?");
-            }
-            if (name == "Current status")
-			{
-				_serial_port.Send("APPLY:OUTput?");
-			}
-            else if(name == "Current value")
-            {
-				_serial_port.Send("APPLY:CURRent:LEVel?");
-			}
-        }
-
-		private string receive_value_from_supply(string name)
-		{
-            if(_serial_port == null)
-                return null;
-
-			string internal_buffer;
-			_serial_port.Read(out internal_buffer);
-
-            if (string.IsNullOrEmpty(internal_buffer))
-                return null;
-
-
-			return internal_buffer;
-		}
 
 		#endregion Methods
 
