b4723d57b310cec4cdef8606fbcec0d240351634
Smadar Tsdaka <smadar@irpsystems.com>
2024-03-14 14:14:41 +0200

    Fixed mix up with Dyno3

--- DeviceCommunicators/NI_6002/NI6002_ParamData.cs
+++ DeviceCommunicators/NI_6002/NI6002_ParamData.cs
@@ -6,12 +6,12 @@ using System.Collections.Generic;
 namespace DeviceCommunicators.NI_6002
 {
 
-    public class Dyno3ParamData : DeviceParameterData, IParamWithDropDown
-    { 
-        public int  Io_port { set; get; } // select port 
-        public string Status_paramter { set; get; }// Read only (R) or write (RW)
-        public string command_to_device { set; get; }
+	public class NI6002_ParamData : DeviceParameterData, IParamWithDropDown
+	{
+		public int Io_port { set; get; } // select port 
+		public string Status_paramter { set; get; }// Read only (R) or write (RW)
+		public string command_to_device { set; get; }
 
-        public List<DropDownParamData> DropDown { get; set; }
-    }
+		public List<DropDownParamData> DropDown { get; set; }
+	}
 }
