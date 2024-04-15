54a84e93f897f16cbfbab4b2761fae1eeef17f83
Smadar Tsdaka <smadar@irpsystems.com>
2024-04-15 12:02:09 +0300

    Add properties regarding the DropDown to PowerSupplayGK_ParamData

--- DeviceCommunicators/PowerSupplayGK/PowerSupplayGK_ParamData.cs
+++ DeviceCommunicators/PowerSupplayGK/PowerSupplayGK_ParamData.cs
@@ -1,6 +1,8 @@
 ﻿
 using DeviceCommunicators.Models;
 using Entities.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace DeviceCommunicators.PowerSupplayGK
@@ -11,6 +13,59 @@ namespace DeviceCommunicators.PowerSupplayGK
 		public ushort WriteAddress { get; set; }
 		public ushort WriteTriggerAddress { get; set; }
 		public double Scale { get; set; }
+
+
+
+
+
+		private bool _isSettingValue;
+		public override object Value
+		{
+			get
+			{
+				
+				return _value;
+
+			}
+			set
+			{
+
+				_value = value;
+				if (_isSettingSelectedDropDown)
+					return;
+
+				_isSettingValue = true;
+				if (DropDown != null)
+				{
+					SelectedDropDown = DropDown.Find((dd) => dd.Name == (_value as string));
+					if(SelectedDropDown == null)
+						SelectedDropDown = DropDown.Find((dd) => dd.Value == (_value.ToString()));
+
+				}
+				_isSettingValue = false;
+
+			}
+		}
+
+		private bool _isSettingSelectedDropDown;
+		private DropDownParamData _selectedDropDown;
+		[JsonIgnore]
+		public DropDownParamData SelectedDropDown
+		{
+			get => _selectedDropDown;
+			set
+			{
+				_selectedDropDown = value;
+				if (_isSettingValue)
+					return;
+
+				_isSettingSelectedDropDown = true;
+				int nVal;
+				bool res = int.TryParse(_selectedDropDown.Value, out nVal);
+				Value = nVal;
+				_isSettingSelectedDropDown = false;
+			}
+		}
 
 		public List<DropDownParamData> DropDown { get; set; }
 	}
