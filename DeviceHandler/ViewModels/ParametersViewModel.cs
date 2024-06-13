eaf04387bacc342087bd2efb62b8750c91195a6e
Smadar Tsdaka <smadar@irpsystems.com>
2024-06-13 13:48:43 +0300

    For MCU, use Cmd to identify parameter.

--- DeviceHandler/ViewModels/ParametersViewModel.cs
+++ DeviceHandler/ViewModels/ParametersViewModel.cs
@@ -179,9 +179,13 @@ namespace DeviceHandler.ViewModel
 
 					DeviceFullData deviceFullData = _devicesContainer.TypeToDevicesFullData[param.DeviceType];
 					if (deviceFullData == null)
-						return;					
+						return;
 
-					actualParam = deviceFullData.Device.ParemetersList.ToList().Find((p) => p.Name == param.Name);
+					if (param is MCU_ParamData mcuParam)
+						actualParam = deviceFullData.Device.ParemetersList.ToList().Find((p) => ((MCU_ParamData)p).Cmd == mcuParam.Cmd);
+					else
+						actualParam = deviceFullData.Device.ParemetersList.ToList().Find((p) => p.Name == param.Name);
+
 					if (actualParam == null)
 						return;
 
