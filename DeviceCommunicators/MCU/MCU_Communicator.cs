186e3232bd177466a0595bd8e884047d22187ae1
Smadar Tsdaka <smadar@irpsystems.com>
2024-06-19 16:03:29 +0300

    CanService take ID for send and received

--- DeviceCommunicators/MCU/MCU_Communicator.cs
+++ DeviceCommunicators/MCU/MCU_Communicator.cs
@@ -110,15 +110,15 @@ namespace DeviceCommunicators.MCU
 
 			if (canAdapterType == "PCAN")
 			{
-				CommService = new CanPCanService(baudrate, hwId, syncID);
+				CommService = new CanPCanService(baudrate, hwId, syncID, syncID);
 			}
 			else if (canAdapterType == "Sloki")
 			{
-				CommService = new CanSlokiService(baudrate, syncID);
+				CommService = new CanSlokiService(baudrate, syncID, syncID);
 			}
 			else if (canAdapterType == "UDP Simulator")
 			{
-				CommService = new CanUdpSimulationService(baudrate, syncID, rxPort, txPort, address);
+				CommService = new CanUdpSimulationService(baudrate, syncID, syncID, rxPort, txPort, address);
 			}
 
 
