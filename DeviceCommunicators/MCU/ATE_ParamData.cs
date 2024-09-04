a61775b924362c126ab219e1f2e8edb27e735695
Smadar Tsdaka <smadar@irpsystems.com>
2024-09-04 14:01:26 +0300

    Add ATE_ParamData

---
+++ DeviceCommunicators/MCU/ATE_ParamData.cs
@@ -0,0 +1,25 @@
+﻿
+using Entities.Models;
+using System.Collections.Generic;
+using System;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using DeviceCommunicators.Models;
+
+namespace DeviceCommunicators.MCU
+{
+
+	
+	public class ATE_ParamData : MCU_ParamData
+	{
+		public List<DropDownParamData> ATECommand { get; set; }
+
+	}
+
+	
+
+	
+}
