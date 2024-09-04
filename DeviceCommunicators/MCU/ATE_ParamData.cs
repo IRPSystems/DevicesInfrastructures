907c5c16b550a2daa78d41581e33e252f66702c2
Smadar Tsdaka <smadar@irpsystems.com>
2024-09-04 14:43:27 +0300

    Code cosmetics

--- DeviceCommunicators/MCU/ATE_ParamData.cs
+++ DeviceCommunicators/MCU/ATE_ParamData.cs
@@ -1,13 +1,6 @@
 ﻿
 using Entities.Models;
 using System.Collections.Generic;
-using System;
-using System.Windows;
-using System.Collections.ObjectModel;
-using System.Security.Cryptography;
-using System.Text;
-using Newtonsoft.Json;
-using DeviceCommunicators.Models;
 
 namespace DeviceCommunicators.MCU
 {
