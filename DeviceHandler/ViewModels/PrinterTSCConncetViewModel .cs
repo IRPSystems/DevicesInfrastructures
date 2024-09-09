74368676698403175bc364890e00d58c4e9cf179
ofriznaty <ofri@irpsystems.com>
2024-09-09 11:39:19 +0300

    Finished tsc connection logic

--- DeviceHandler/ViewModels/PrinterTSCConncetViewModel .cs
+++ DeviceHandler/ViewModels/PrinterTSCConncetViewModel .cs
@@ -15,7 +15,7 @@ namespace DeviceHandler.ViewModels
 	{
 		[JsonIgnore]
 
-		public ObservableCollection<string> DeviceList { get; set; }
+		private ObservableCollection<string> DeviceList = new ObservableCollection<string>();
 
 		public bool IsConnectButtonEnabled { get; set; }
 		[JsonIgnore]
@@ -44,28 +44,28 @@ namespace DeviceHandler.ViewModels
 
 		private void BuildDetectedDevicesList()
 		{
-            // Set the query to retrieve printers from Device Manager
+			// Set the query to retrieve printers from Device Manager
 
-            //string query = "SELECT * FROM Win32_Printer";
+			string query = "SELECT * FROM Win32_Printer";
 
-            //// Create a ManagementObjectSearcher with the query
-            //ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+			// Create a ManagementObjectSearcher with the query
+			ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
 
-            //// Perform the query and get the collection of printers
-            //ManagementObjectCollection printers = searcher.Get();
+			// Perform the query and get the collection of printers
+			ManagementObjectCollection printers = searcher.Get();
 
-            //////Iterate over the printers and add their names to the list, add only connected printers
-            ////foreach (ManagementObject printer in printers)
-            ////{
-            ////    string printerName = printer["Name"] as string;
-            ////    string printerStatus = printer["PrinterStatus"]?.ToString();
-            ////    if (printerName.Contains("TSC"))
-            ////    {
-            ////        DeviceName = printerName;
-            ////        DeviceList.Add(printerName);
-            ////    }
-            ////}
-        }
+			//Iterate over the printers and add their names to the list, add only connected printers
+			foreach (ManagementObject printer in printers)
+			{
+				string printerName = printer["Name"] as string;
+				string printerStatus = printer["PrinterStatus"]?.ToString();
+				if (printerName.Contains("TSC"))
+				{
+					DeviceName = printerName;
+					DeviceList.Add(printerName);
+				}
+			}
+		}
 
 		public void RefreshProperties() { }
 
