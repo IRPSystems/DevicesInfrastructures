5c304680dfa9f35719d77fee40933f05f34d9af0
Smadar Tsdaka <smadar@irpsystems.com>
2024-09-15 14:51:23 +0300

    Handle FaultsMCUView Closing and Loaded

--- DeviceHandler/Faults/FaultsMCUView.xaml.cs
+++ DeviceHandler/Faults/FaultsMCUView.xaml.cs
@@ -14,8 +14,21 @@ namespace DeviceHandler.Faults
 
 		private void MetroWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
 		{
+			if (DataContext is FaultsMCUViewModel faultsMCUViewModel)
+			{
+				faultsMCUViewModel.ClosingCommand?.Execute(null);
+			}
+
 			e.Cancel = true;
 			Hide();
 		}
-	}
+
+		private void MetroWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+		{
+			if (DataContext is FaultsMCUViewModel faultsMCUViewModel)
+			{
+				faultsMCUViewModel.LoadedCommand?.Execute(null);
+			}
+		}
+    }
 }
